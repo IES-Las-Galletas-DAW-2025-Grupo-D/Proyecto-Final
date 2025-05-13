2b0594a46cbf0e47ee38dde1c4843d965eecb7f1
TeRacksito <acho.chuchi.8@gmail.com>
2025-05-13 10:57:32 +0000

    first dashboard iteration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from "react-router";
-import { FullLayout } from "./layout/FullLayout";
+import { FullLayout } from "./components/layouts/full/FullLayout";
 import { HomePage } from "./Home/HomePage";
+import { DashboardPage } from "./pages/Dashboard/DashboardPage";
 
 export function App() {
   return (
-    <FullLayout>
+    <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <FullLayout>
+              <HomePage />
+            </FullLayout>
+          }
+        />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
-    </FullLayout>
+    </>
   );
 }
