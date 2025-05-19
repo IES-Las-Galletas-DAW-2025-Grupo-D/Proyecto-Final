b06bda26fb57249cb1c0d2e72f7a759bdde89056
TeRacksito <acho.chuchi.8@gmail.com>
2025-05-19 19:21:09 +0000

    remove svgs

--- frontend/src/components/projects/ProjectsList.tsx
+++ frontend/src/components/projects/ProjectsList.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import { getProjects } from "../../services/projects/ProjectService";
 import { PaginatedResponse } from "../../types/paginated";
 import { Project } from "../../types/project";
-import { ProjectCard } from "./ProjectCard"; // Import ProjectCard
+import { ProjectCard } from "./ProjectCard";
+import { FaTimesCircle } from "react-icons/fa";
 
 export function ProjectsList() {
   const [projectsData, setProjectsData] =
@@ -49,19 +50,7 @@ export function ProjectsList() {
   if (error) {
     return (
       <div role="alert" className="alert alert-error">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="stroke-current shrink-0 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M10 14l2-2m0 0l2-2m-2 2l-2 2m2-2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <FaTimesCircle className="shrink-0 h-6 w-6" />
         <span>Error! {error}</span>
       </div>
     );
@@ -122,7 +111,9 @@ export function ProjectsList() {
           )}
         </div>
       ) : (
-        <p className="text-center text-lg text-base-content/70 py-8">No projects found.</p>
+        <p className="text-center text-lg text-base-content/70 py-8">
+          No projects found.
+        </p>
       )}
     </div>
   );
