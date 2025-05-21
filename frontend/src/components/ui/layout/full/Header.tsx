6fec0708acdf08d454f6e43197cd37153f86d9b0
Acoran17 <cristo27603@gmail.com>
2025-05-21 17:25:26 +0000

    redesign in header and homepage(incomplete)

--- frontend/src/components/ui/layout/full/Header.tsx
+++ frontend/src/components/ui/layout/full/Header.tsx
@@ -4,11 +4,11 @@ export function Header() {
 
   return (
     <header className="flex justify-between items-center mb-16">
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100 ">
         <div className="navbar-start">
           <Link to="/" className="btn btn-ghost text-xl">TimeLines</Link>
         </div>
-        <div className="navbar-end">
+        <div className="navbar-end px-4">
           <Link to="/signup" className="btn bg-primary text-primary-content text-base">Signup</Link>
         </div>
     </div>
