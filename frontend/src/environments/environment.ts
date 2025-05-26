7a22dc591a0c674d0d0a533fc6b30d8fc7205974
ermiich <ermichyt@gmail.com>
2025-05-26 01:44:26 +0100

    lil fix

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  apiUrl: "/api/v1",
+  FRONTEND_URL: "http://localhost:3000",
+  // defaultLanguage: 'en',
+  // supportedLanguages: ['en', 'es'],
+  // defaultTheme: 'light',
+  // supportedThemes: ['light', 'dark'],
+};
